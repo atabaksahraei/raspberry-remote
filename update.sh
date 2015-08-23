8e151378f1fc937b0bf9f0eaa8d5cf1e6d3e4ff8
Atabak Sahraei <atabaksahraei@atabaks-mbp.speedport_w_724v_09011603_00_012>
2015-08-23 03:04:37 +0200

    bugfix

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo rm -r /var/www/at/
+sudo mkdir /var/www/at
+rm -r ~/raspberry-remote
+git clone git://github.com/atabaksahraei/raspberry-remote.git
+sudo mv ~/raspberry-remote/webinterface/* /var/www/at
