a1851a6415ae438977f62eda626cffd04600c49e
Atabak Sahraei <atabaksahraei@atabaks-mbp.speedport_w_724v_09011603_00_012>
2015-08-23 03:10:25 +0200

    bugfix

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ sudo mkdir /var/www/at
 rm -r ~/raspberry-remote
 git clone git://github.com/atabaksahraei/raspberry-remote.git
 sudo mv ~/raspberry-remote/webinterface/* /var/www/at
-make ~/raspberry-remote/send
-make ~/raspberry-remote/daemon
-sudo ~/raspberry-remote/daemon
+cd ~/raspberry-remote/
+make send
+make daemon
+sudo ./daemon
+cd ~
