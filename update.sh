87546e8f7f0c46848e794a8ddfe8b5d5fbd03f5a
Atabak Sahraei <atabak@sahraei.de>
2015-08-25 17:58:33 +0200

    moved all cpp stuff to core folder

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+echo clean
 sudo rm -r /var/www/at/
-sudo mkdir /var/www/at
 rm -r ~/raspberry-remote
+echo clone git repo
 git clone git://github.com/atabaksahraei/raspberry-remote.git
+echo copy webservice files
+sudo mkdir /var/www/at
 sudo cp ~/raspberry-remote/webinterface/* /var/www/at
-cd ~/raspberry-remote/
+cd ~/raspberry-remote/core
+echo compile: send
 make send
+echo compile: daemon
 make daemon
+echo run daemon
 sudo ./daemon
 cd ~
