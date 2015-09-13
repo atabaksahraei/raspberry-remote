6c3efbf260268f84c80e89cf41793ef5129a549c
Atabak Sahraei <atabak@sahraei.de>
2015-09-13 22:56:49 +0200

    scrpit fix

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd ~/raspberry-remote
 ./cleanPort.sh
 cd ~/raspberry-remote/core
-sudo .daemon.sh &> /myOutput.log  &
+sudo ./daemon.sh &> myOutput.log  &
 echo $! >> my_pids.log
 echo "PID of myJob.sh is: $!  - Saved to my_pids.log"
 exit 0
