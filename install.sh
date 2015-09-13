9e9d7627606965239738e11f8e9e7a6282dde211
Atabak Sahraei <atabak@sahraei.de>
2015-09-13 13:17:07 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install git-core~
-d ~
+sudo apt-get install git-core
+cd ~
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build
 cd ~
 git clone git://github.com/atabaksahraei/raspberry-remote.git
-cp ~raspberry-remote/update ~update
+cp ~/raspberry-remote/update ~/update
 cd ~
 ./update
